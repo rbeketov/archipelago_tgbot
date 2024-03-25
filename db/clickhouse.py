bb86438b1175c20aeeb14ebd8ed3993fabab64db
rbeketov <problemkarlfree@gmail.com>
2024-03-25 15:27:29 +0300

    fix query click and add common config

--- db/clickhouse.py
+++ db/clickhouse.py
@@ -2,6 +2,7 @@ import os
 
 
 import clickhouse_connect
+from clickhouse_connect import common
 from dotenv import load_dotenv
 
 
@@ -24,6 +25,8 @@ class ClickClient(metaclass=SingleTone):
     def __init__(self):
         load_dotenv()
 
+        common.set_setting('autogenerate_session_id', False)
+
         self._host = os.environ.get(HOST)
         self._port = os.environ.get(PORT)
         self._database = os.environ.get(DATABASE)
@@ -31,9 +34,8 @@ class ClickClient(metaclass=SingleTone):
             host=self._host,
             port=self._port,
             username='default',
+            database=self._database,
         )
-
-        self._click_client.command(f'USE {self._database}')
 
     def insert_new_message(
         self,
@@ -52,7 +54,10 @@ class ClickClient(metaclass=SingleTone):
         chat_id: int,
     ):
         query_ = f"SELECT * FROM tg_messages WHERE chat_id = {chat_id}"
-        return self._click_client.query(query_).result_rows
+        q = self._click_client.query(query_)
+        res = q.result_rows
+        q.close()
+        return res
     
     def insert_or_update_token(
         self,
@@ -63,7 +68,8 @@ class ClickClient(metaclass=SingleTone):
         result_select = self._click_client.query(query_).result_rows
         if result_select:
             query_upd = f"UPDATE notes_chat_relations SET chat_id = {chat_id} WHERE note_id = {note_token};"
-            self._click_client.query(query_upd)
+            q = self._click_client.query(query_upd)
+            q.close()
         else:
             data = [[note_token, chat_id]]
             self._click_client.insert('notes_chat_relations', data, column_names=['note_id', 'chat_id'])
@@ -73,7 +79,9 @@ class ClickClient(metaclass=SingleTone):
         note_token: int,
     ):
         query_ = f"SELECT * FROM notes_chat_relations WHERE note_id = {note_token}"
-        result_select = self._click_client.query(query_).result_rows
+        q = self._click_client.query(query_)
+        result_rows = q.result_rows
+        q.close()
         if result_select:
             return result_select[0][-1]
         return None
